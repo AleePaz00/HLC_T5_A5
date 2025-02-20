1fe9f747ffe2697f4ca219f108ab8779ee3fd601
AleePaz00 <apazram720@g.educaand.es>
2025-02-20 08:55:31 +0100

    Add files via upload

--- Ej5.py
+++ Ej5.py
@@ -1,23 +1,20 @@
 # Crea una clase Figura con un método calcular_area() 
 # vacío (pass). Luego, crea las clases Circulo y 
 # Rectangulo que implementen este método.
+
 import math
 class Figura:
     def calcular_area(self):
         pass
-
 class Circulo(Figura):
     def __init__(self, radio):
         self.radio = radio
-
     def calcular_area(self):
         return math.pi * (self.radio ** 2)
-
 class Rectangulo(Figura):
     def __init__(self, ancho, alto):
         self.ancho = ancho
         self.alto = alto
-
     def calcular_area(self):
         return self.ancho * self.alto
 c = Circulo(5)
